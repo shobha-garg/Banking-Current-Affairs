f67cd9c11e20dadbe8327e6d872b74c72079c17d
Shobha Garg <gargshobha5@gmail.com>
2025-05-04 21:57:54 +0530

    Ready to host

--- app/app.py
+++ app/app.py
@@ -355,4 +355,5 @@ def get_stats():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create tables on startup
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
--- app/backend/config.py
+++ app/backend/config.py
@@ -5,4 +5,4 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = '84dbd219dbcca789bfeaa252c51a8d0526f184f96c9246f45ef96561abfb6110'  
-    DEBUG = True
+    DEBUG = False
