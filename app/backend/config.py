8b5891a06583846e056e8e1746b5072e81e2c4db
Shobha Garg <gargshobha5@gmail.com>
2025-05-04 22:06:04 +0530

    removed unwanted file

--- app/backend/config.py
+++ app/backend/config.py
@@ -4,5 +4,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'quiz.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SECRET_KEY = '84dbd219dbcca789bfeaa252c51a8d0526f184f96c9246f45ef96561abfb6110'  
+    SECRET_KEY = 'your_secret_key'  
     DEBUG = False
